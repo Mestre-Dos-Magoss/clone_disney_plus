75a9ea6b54f5a86406d9790a99292e66c36d851b
Mestre dos Magos <ferminoapolinariojuliano@gmail.com>
2025-04-18 15:49:49 -0300

    Criando o Hero e implementando as fonts customizadas

--- gulpFile.js
+++ gulpFile.js
@@ -1,15 +1,24 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
 
+// // "**" tudo o que tiver "Usamos mais para pastas"
+// //"*" com "Usamos mais para extensoes de arquivos"
 function compilaSass(){
-    return gulp.src("src/styles/*.scss")
-    .pipe(sass({
-        outputStyle:"compressed"
-    }))
-    .pipe(gulp.dest("./dist/styles"));
+   return gulp.src("src/styles/**/*.scss")
+   .pipe(sass({outputStyle:"compressed"}))
+   .pipe(gulp.dest("dist/styles"));
 }
 
-exports.default = function(){
-    gulp.watch("src/styles/*scss", compilaSass)
+function comprimeImagens(){
+   return gulp.src("src/imgs/images/**/*",{encoding:false})
+   .pipe(imagemin())
+   .pipe(gulp.dest("dist/imgs/images"));
 }
+exports.default = gulp.parallel(comprimeImagens,compilaSass);
+
+exports.watch = function(){
+   gulp.watch("src/styles/**/*.scss", gulp.parallel(compilaSass));
+}
+
